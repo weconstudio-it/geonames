6b7928fb874aef39e8c58269100b8a51a821878f
alessandro.cabutto <alessandro.cabutto@gmail.com>
2016-04-27 10:33:00 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # geonames
-Import geonames.org data to MySQL 
+Import geonames.org data to a MySQL database.
+
+#Usage
+-----
+First of all you need a working MySQL instance on localhost accessible by local
+user without authentication (e.g. using ~/.my.cnf file).
+
+Then you only need to launch the geonames_import.sh script:
+
+	$./geonames_import.sh
+
+#Remarks
+-----
+Geonames data is downloaded from geonames.org. The whole process may take a 
+while depending on your bandwidth and machine performances.
---
+++ geonames_import.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILES=('allCountries.zip' 'alternateNames.zip' 'admin2Codes.txt' 'admin1CodesASCII.txt' 'featureCodes_en.txt' 'timeZones.txt' 'countryInfo.txt' 'shapes_simplified_low.zip');
+
+[ -e data ] || mkdir data
+
+cd data
+
+for FILE in "${FILES[@]}"; do
+	echo "Downloading $FILE..."
+	[ -e $FILE ] || curl -O http://download.geonames.org/export/dump/{$FILE}
+done
+
+[ -e allCountries.txt ] || unzip allCountries.zip
+[ -e alternateNames.txt ] || unzip alternateNames.zip
+[ -e shapes_simplified_low.txt ] || unzip shapes_simplified_low.zip
+cat countryInfo.txt | grep -v "^#" >countryInfo-n.txt 
+
+cd ..
+
+DATA_PATH=$(pwd)
+
+sed -e "s@PATH_PLACEHOLDER@$DATA_PATH@g" import.sql.template > tmp_import.sql
+
+echo "Importing data..."
+
+mysql --local-infile <tmp_import.sql
+
+rm tmp_import.sql
