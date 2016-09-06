3483c85f5addec69d2a1065278ec74366a3c3145
root <root@server07.weconstudio.it>
2016-09-06 17:44:38 +0200

    Fix

--- geonames_import.sh
+++ geonames_import.sh
@@ -2,7 +2,9 @@
 
 #FILES=('allCountries.zip' 'alternateNames.zip' 'admin2Codes.txt' 'admin1CodesASCII.txt' 'featureCodes_en.txt' 'timeZones.txt' 'countryInfo.txt' 'shapes_simplified_low.zip');
 
-FILES=('allCountries.zip' 'alternateNames.zip' 'admin2Codes.txt' 'admin1CodesASCII.txt' 'featureCodes_en.txt' 'timeZones.txt' 'countryInfo.txt' 'shapes_simplified_low.zip');
+#FILES=('allCountries.zip' 'alternateNames.zip' 'admin2Codes.txt' 'admin1CodesASCII.txt' 'featureCodes_en.txt' 'timeZones.txt' 'countryInfo.txt' 'shapes_simplified_low.zip');
+
+FILES=('admin2Codes.txt')
 
 [ -e data ] || mkdir data
 
@@ -13,19 +15,22 @@ for FILE in "${FILES[@]}"; do
 	[ -e $FILE ] || curl -O http://download.geonames.org/export/dump/{$FILE}
 done
 
-[ -e allCountries.txt ] || unzip allCountries.zip
-[ -e alternateNames.txt ] || unzip alternateNames.zip
-[ -e shapes_simplified_low.txt ] || unzip shapes_simplified_low.zip
-cat countryInfo.txt | grep -v "^#" >countryInfo-n.txt 
+[ -e allCountries.zip ] &&  unzip allCountries.zip
+[ -e alternateNames.zip ] && unzip alternateNames.zip
+[ -e shapes_simplified_low.zip ] && unzip shapes_simplified_low.zip
+
+[ -e countryInfo.txt ] && cat countryInfo.txt | grep -v "^#" >countryInfo-n.txt 
 
 cd ..
 
 DATA_PATH=$(pwd)
 
-sed -e "s@PATH_PLACEHOLDER@$DATA_PATH@g" import.sql.template > tmp_import.sql
+#sed -e "s@PATH_PLACEHOLDER@$DATA_PATH@g" import.sql.template > tmp_import.sql
+[ -e data/admin2Codes.txt ] && sed -e "s@PATH_PLACEHOLDER@$DATA_PATH@g" admin2_code.sql.template > tmp_admin2_code.sql
 
 echo "Importing data..."
 
-mysql -u root -p --local-infile <tmp_import.sql
+#mysql -u root  --local-infile <tmp_import.sql
+mysql -u root  --local-infile <tmp_admin2_code.sql
 
-rm tmp_import.sql
+rm -f tmp*.sql
